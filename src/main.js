dbb53e1ffb6a116748f9b38ae4d62dd456a56ecc
asadullah055 <asadullahahmed01@gmail.com>
2023-03-03 23:43:04 +0600

    update menu

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ const cart = document.getElementById("cart");
 const closeBtn = document.getElementById("close-btn");
 const sideBox = document.getElementById("side-box");
 const overlay = document.getElementById("overlay");
+const btn = document.getElementById("btn");
+const closeMenu = document.getElementById("closeBtn");
 
-// add function
+// add function side bar
 let openBar = function () {
   sidebar.style.right = "0px";
   document.body.classList = "open";
   overlay.classList.add("overlay");
 };
-// remove function
+// remove function side bar
 let closeBar = function () {
-  sidebar.style.right = "-500px";
+  sidebar.style.right = "-600px";
   document.body.classList.remove("open");
   overlay.classList.remove("overlay");
 };
@@ -20,3 +22,10 @@ cart.addEventListener("click", openBar);
 sideBox.addEventListener("click", openBar);
 closeBtn.addEventListener("click", closeBar);
 overlay.addEventListener("click", closeBar);
+// ==================
+btn.addEventListener("click", function () {
+  document.getElementById("main-menu").style.left = "0";
+});
+closeMenu.addEventListener("click", function () {
+  document.getElementById("main-menu").style.left = "-500px";
+});
